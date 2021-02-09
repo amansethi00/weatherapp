a521466e2c42801bb6098ab51e3a8260506e4e23
Aman Sethi <35420536+amansethi00@users.noreply.github.com>
2021-02-09 22:09:54 +0530

    design:add glassmorphism

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {axios} from "axios";
 import {
   faSearchLocation,
   faWind,
   faTint,
+  faFlag,
 } from "@fortawesome/free-solid-svg-icons";
 import {RingLoader} from "react-spinners";
 
@@ -15,10 +17,12 @@ function App() {
   const [weather, setWeather] = useState("");
   const [JSON, setJSON] = useState({});
   const [bgImage, setbgImage] = useState("");
+
   function inputHandler(e) {
     setuserInput(e.target.value);
     // console.log(e.target.value);
   }
+
   function handleKeyDown(e) {
     if (e.key === "Enter" && userInput !== "") {
       setShowLoader(true);
@@ -39,6 +43,9 @@ function App() {
                   "@2x.png",
                 temp: json.main.temp,
                 main: json.weather[0].main,
+                humidity: json.main.humidity,
+                wind: json.wind.speed,
+                country: json.sys.country,
               });
               setWeather(parseInt(json.main.temp) - 273);
               setbgImage(json.weather[0].main);
@@ -70,14 +77,13 @@ function App() {
           ></input>
         </div>
       </div>
-      <div className="body-container">
-        <RingLoader
-          loading={showLoader}
-          color="orange"
-          className="loader"
-        ></RingLoader>
-        {weather !== "" ? (
-          <>
+      <div className="loader">
+        <RingLoader loading={showLoader} color="orange"></RingLoader>
+      </div>
+
+      {weather !== "" ? (
+        <>
+          <div className="body-container">
             <div className="fetchedData">
               <div className="body-head">
                 <img src={JSON.icon} alt="weather logo" className="icon"></img>
@@ -91,23 +97,29 @@ function App() {
                     icon={faWind}
                     style={{marginRight: "0.5rem"}}
                   ></FontAwesomeIcon>{" "}
-                  wind
+                  {JSON.wind}km/h
                 </span>
                 <span>
                   <FontAwesomeIcon
                     icon={faTint}
                     style={{marginRight: "1rem"}}
                   />
-                  humidity
+                  {JSON.humidity}
                 </span>
-                <span>sunlight</span>
+                <span>
+                  <FontAwesomeIcon
+                    icon={faFlag}
+                    style={{marginRight: "0.8rem"}}
+                  />
+                  {JSON.country}
+                </span>
               </div>
             </div>
-          </>
-        ) : (
-          <></>
-        )}
-      </div>
+          </div>
+        </>
+      ) : (
+        <></>
+      )}
     </div>
   );
 }
